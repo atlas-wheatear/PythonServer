d67f3a87d0e6065352fc6f94274b4246ddaae330
Edward Moulsdale <eddie.moulsdale@gmail.com>
2020-07-21 16:06:06 +0100

    Added jinja templating

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+count = 1
+
 @app.route("/")
 def hello():
-    return "Good day, oblate spheroid"
+    global count
+    template = render_template("index.html", viewer_number=count)
+    count += 1
+    return template
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
